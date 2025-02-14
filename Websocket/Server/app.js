2b0b21109e13670fec3f51e437548ff2a333d53f
SaptatirthaChoudhury <saptatirtha@gmail.com>
2025-02-15 00:12:03 +0530

    modified

--- Websocket/Server/app.js
+++ Websocket/Server/app.js
@@ -11,19 +11,15 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 wss.on("connection", (ws) => {
-
   //connection is up, let's add a simple event
   ws.on("message", (message) => {
-
     //log the received message and send it back to the client
     console.log("received: %s", message);
     ws.send(`Hello, you sent -> ${message}`);
-
   });
 
   //send immediatly a feedback to the incoming connection
   ws.send("Hi there, I am a WebSocket server");
-  
 });
 
 //start our server
